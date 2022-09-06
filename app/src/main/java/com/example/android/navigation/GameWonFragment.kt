f319297d620290645c8d012273f762df80f70635
Adham Nour <adhamnourelwaffaa@gmail.com>
2022-09-06 20:02:02 +0200

    finished step 21

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -33,6 +34,8 @@ class GameWonFragment : Fragment() {
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
         binding.nextMatchButton.setOnClickListener { view -> view.findNavController().popBackStack() }
+        var args = GameWonFragmentArgs.fromBundle(arguments!!)
+        Toast.makeText(context, args.toString(), Toast.LENGTH_SHORT).show()
         return binding.root
     }
 }
